aeacf2734e3b9ed181cb551a66ab9e24a323c92c
p7lejo <117734130+p7lejo@users.noreply.github.com>
2026-09-18 09:15:48 +0200

    Add initial Bluetooth discovery integration

---
+++ custom_components/oregon_idtw21r/const.py
@@ -0,0 +1,5 @@
+"""Constants for the Oregon Scientific IDTW21xR integration."""
+
+DOMAIN = "oregon_idtw21r"
+NAME = "Oregon Scientific IDTW21xR"
+SERVICE_UUID = "74e7fe00-c6a4-11e2-b7a9-0002a5d5c51b"
