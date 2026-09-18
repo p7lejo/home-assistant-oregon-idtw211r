384e3e8de6c5a94a1f919deece9ed3cdedfaac63
p7lejo <117734130+p7lejo@users.noreply.github.com>
2026-09-18 13:12:43 +0200

    Fix Bluetooth connection constant import

--- custom_components/oregon_idtw21r/sensor.py
+++ custom_components/oregon_idtw21r/sensor.py
@@ -4,12 +4,11 @@ from __future__ import annotations
 
 from homeassistant.components.sensor import SensorDeviceClass, SensorEntity
 from homeassistant.const import (
-    CONNECTION_BLUETOOTH,
     PERCENTAGE,
     SIGNAL_STRENGTH_DECIBELS_MILLIWATT,
     UnitOfTemperature,
 )
-from homeassistant.helpers.device_registry import DeviceInfo
+from homeassistant.helpers.device_registry import CONNECTION_BLUETOOTH, DeviceInfo
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
 from .const import DOMAIN, NAME
